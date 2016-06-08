976abf5f615a17929e515d0e912b7d9f2ab15d0b
lalithc <lchennup@gmail.com>
2016-06-07 21:46:52 -0700

    ADDED FEW MORE REQUIRED FILES

--- simple-weather/src/main/java/org/sonatype/mavenbook/weather/WeatherService.java
+++ simple-weather/src/main/java/org/sonatype/mavenbook/weather/WeatherService.java
@@ -7,13 +7,13 @@ public class WeatherService {
     public WeatherService() {}
 
     public String retrieveForecast( String zip ) throws Exception {
-        // Retrieve Data
-        InputStream dataIn = new YahooRetriever().retrieve( zip );
-
-        // Parse Data
-        Weather weather = new YahooParser().parse( dataIn );
-
-        // Format (Print) Data
-        return new WeatherFormatter().format( weather );
-    }
+	// Retrieve Data
+	InputStream dataIn = new YahooRetriever().retrieve( zip );
+	
+	// Parse Data
+	Weather weather = new YahooParser().parse( dataIn );
+	
+	// Format (Print) Data
+	return new WeatherFormatter().format( weather );
+    }	
 }
--- simple-webapp/src/main/java/org/sonatype/mavenbook/web/WeatherServlet.java
+++ simple-webapp/src/main/java/org/sonatype/mavenbook/web/WeatherServlet.java
@@ -9,14 +9,14 @@ public class WeatherServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
-        String zip = request.getParameter("zip" );
-        WeatherService weatherService = new WeatherService();
-        PrintWriter out = response.getWriter();
+	String zip = request.getParameter("zip" );
+	WeatherService weatherService = new WeatherService();
+	PrintWriter out = response.getWriter();
         try {
-            out.println( weatherService.retrieveForecast( zip ) );
-        } catch( Exception e ) {
-            out.println( "Error Retrieving Forecast: " + e.getMessage() );
-        }
+	    out.println( weatherService.retrieveForecast( zip ) );
+	} catch( Exception e ) {
+	    out.println( "Error Retrieving Forecast: " + e.getMessage() );
+	}
         out.flush();
         out.close();
     }
